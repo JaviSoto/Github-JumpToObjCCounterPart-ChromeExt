20eb02ce52fc0a1f308cd0e8e7e2085c6f97e93e
Javier Soto <javiers@twitter.com>
2015-02-28 15:08:53 -0800

    Initial source + extension commit

---
+++ src/events.js
@@ -0,0 +1,41 @@
+function jumpToCounterPathFile() {
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tab) {
+    tab = tab[0];
+
+    if (tab === undefined) {
+      console.log("Couldn't get the current tab");
+      return;
+    }
+
+    var tabURL = tab.url;
+
+    var URLIsInGithubDomain = (tabURL.indexOf("https://github.com/") === 0);
+    if (!URLIsInGithubDomain) {
+      return;
+    }
+
+    var URLIsObjCClassURLRegExp = /(.*\.)(h|m)/;
+    var URLAndObjCExtension = URLIsObjCClassURLRegExp.exec(tabURL);
+
+    if (URLAndObjCExtension !== null) {
+      var URLWithoutExtension = URLAndObjCExtension[1];
+      var extension = URLAndObjCExtension[2];
+
+      var newExtension = extension == "h" ? "m" : "h";
+
+      var newURL = URLWithoutExtension + newExtension;
+
+      chrome.tabs.update(tab.id, { url: newURL});
+    }
+  });
+}
+
+
+chrome.commands.onCommand.addListener(function(command) {
+      if (command == "jump-to-counter-part") {
+        jumpToCounterPathFile();
+      }
+      else {
+        throw "Unknown command " + command;
+      }
+});
